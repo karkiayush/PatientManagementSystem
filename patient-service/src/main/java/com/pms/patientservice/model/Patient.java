10965db5e0406c8962ff28eeb7263aaa01cf04cb
karkiayush <karkiayush619@gmail.com>
2025-04-10 19:03:27 +0545

    configured the h2 database for dev env

---
+++ patient-service/src/main/java/com/pms/patientservice/model/Patient.java
@@ -0,0 +1,50 @@
+package com.pms.patientservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @NotNull
+    private String firstName;
+    @NotNull
+    private String lastName;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private LocalDate birthDate;
+
+    private int age;
+
+    @NotNull
+    private String gender;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private String phoneNumber;
+
+    @NotNull
+    private LocalDate registrationDate;
+}
