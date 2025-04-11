be174a625fc92f270c1354f0564edf56087268bb
karkiayush <karkiayush619@gmail.com>
2025-04-11 10:31:13 +0545

    handle errors using GlobalException for patient-service

---
+++ patient-service/src/main/java/com/pms/patientservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.pms.patientservice.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    /*This will handle any of the validation errors which gets triggered when the jpa validates the request dto in request*/
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException e) {
+        Map<String, String> errorMap = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach(
+                error -> errorMap.put(error.getField(), error.getDefaultMessage())
+        );
+        return ResponseEntity.badRequest().body(errorMap);
+    }
+}
