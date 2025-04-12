8da577a27dac75742529fd17c062bc7f2f12cccf
karkiayush <karkiayush619@gmail.com>
2025-04-12 20:40:40 +0545

    feat: integrate OpenAPI Swagger UI for API documentation

--- patient-service/src/main/java/com/pms/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/pms/patientservice/controller/PatientController.java
@@ -2,14 +2,12 @@ package com.pms.patientservice.controller;
 
 import com.pms.patientservice.dto.PatientRequestDTO;
 import com.pms.patientservice.dto.PatientResponseDTO;
-import com.pms.patientservice.exception.PatientNotFoundException;
-import com.pms.patientservice.mapper.PatientMapper;
-import com.pms.patientservice.model.Patient;
 import com.pms.patientservice.repository.PatientRepository;
 import com.pms.patientservice.service.PatientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,6 +16,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/patients")
+@Tag(name = "Patient", description = "API for managing patients")
 public class PatientController {
     private final PatientService patientService;
     private final PatientRepository patientRepository;
@@ -29,6 +28,7 @@ public class PatientController {
 
     /*CREATE operation*/
     @PostMapping
+    @Operation(summary = "Create a new patient", method = "POST")
     public ResponseEntity<PatientResponseDTO> addPatient(
             /*valid annotation ensures that based on the check case we applied to the PatientRequestDTO, it ensures those are satisfied else it will provide error message*/
             @Valid
@@ -45,6 +45,8 @@ public class PatientController {
 
     /*READ all operation*/
     @GetMapping
+    @Operation(summary = "Get all patients", method = "GET")
+
     public ResponseEntity<List<PatientResponseDTO>> getPatients() {
         List<PatientResponseDTO> patients = patientService.getPatients();
         if (patients.isEmpty()) {
@@ -55,6 +57,7 @@ public class PatientController {
 
     /*READ operation*/
     @GetMapping("/{id}")
+    @Operation(summary = "Get a patient by ID", method = "GET")
     public ResponseEntity<PatientResponseDTO> getPatientById(@PathVariable UUID id) {
         Optional<PatientResponseDTO> patientDto = Optional.ofNullable(patientService.getPatientById(id));
         return patientDto.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
@@ -62,6 +65,7 @@ public class PatientController {
 
     /*UPDATE operation*/
     @PutMapping("/{id}")
+    @Operation(summary = "Update a patient by ID", method = "PUT")
     public ResponseEntity<PatientResponseDTO> updatePatient(@PathVariable UUID id, @RequestBody PatientRequestDTO patientRequestDTO) {
         Optional<PatientResponseDTO> patient = Optional.ofNullable(patientService.updatePatient(id, patientRequestDTO));
         return patient.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
@@ -69,6 +73,7 @@ public class PatientController {
 
     /*DELETE operation*/
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a patient by ID", method = "DELETE")
     public ResponseEntity<PatientResponseDTO> deletePatient(@PathVariable UUID id) {
         Optional<PatientResponseDTO> deletedPatient = Optional.ofNullable(patientService.deletePatient(id));
         return deletedPatient.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
