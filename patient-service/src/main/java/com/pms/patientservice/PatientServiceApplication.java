c6bda78f3e3f0d5c87c2d92637c06f056b0a2f4b
karkiayush <karkiayush619@gmail.com>
2025-04-15 16:09:08 +0545

    style: clean up code comments and formatting

--- patient-service/src/main/java/com/pms/patientservice/PatientServiceApplication.java
+++ patient-service/src/main/java/com/pms/patientservice/PatientServiceApplication.java
@@ -9,16 +9,3 @@ public class PatientServiceApplication {
         SpringApplication.run(PatientServiceApplication.class, args);
     }
 }
-
-/*
- * gRPC(Google Remote Procedure Call)
- * gRPC is a high-performance, open-source universal RPC(Remote Procedure Call) framework that can run in any environment. It is designed for high-performance communication between microservices and supports multiple programming languages.
- *
- * gRPC uses HTTP 2 and Protocol Buffers for efficient low latency communication between its services.
- *
- * gRPC uses HTTP/2 for transport, Protocol Buffers as the interface description language, and it provides features such as authentication, load balancing, and bidirectional streaming.
- *
- * In simple terms, while REST provides an excellent way for browsers & others clients to communicate with services using JSON format, gRPC offers a faster & more efficient method for microservices to interact with each other utilizing proto buff format to send & receive data.
- *
- * While both REST & gRPC uses HTTP under the hood, gRPC is more efficient than REST because it uses HTTP/2 which allows for multiplexing multiple requests over a single connection. This means that gRPC can handle more requests with less overhead compared to REST.
- * */
