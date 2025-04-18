652a40c09cf6c496e0623570bf997afdea58a1f2
karkiayush <karkiayush619@gmail.com>
2025-04-18 17:17:41 +0545

    style: correcting spelling mistakes

--- billing-service/src/main/java/com/pms/billingservice/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/pms/billingservice/grpc/BillingGrpcService.java
@@ -7,7 +7,7 @@ import net.devh.boot.grpc.server.service.GrpcService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/*Here, we store the gRPC server logic, starting the server, receiving messages, sending responses etc...*/
+/*Here, we store the gRPC server logic, starting the server, receiving messages, sending responses, etc...*/
 // This file is the contract for communication between the client and server.
 @GrpcService
 public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
@@ -16,7 +16,7 @@ public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBas
     @Override
     public void createBillingAccount(
             billing.BillingRequest billingRequest,
-            /*once we're ready to respond for the request, we use StreamObserver which is a powerful concept in gRPC as it allows to earn multiple responses to the client at the same time, & also accept back-forth communication to that client*/
+            /*once we're ready to respond to the request, we use StreamObserver, which is a powerful concept in gRPC as it allows earning multiple responses to the client at the same time and also accept back-forth communication to that client*/
             StreamObserver<billing.BillingResponse> responseObserver
     ) {
         log.info("Create billing account request received: {}", billingRequest.toString());
